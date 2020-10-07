54f947e782a174998ea77ada0325fd5290ca1ae5
themarketinglab-jlau <jlau@themarketinglab.com.au>
2020-10-07 10:33:22 +1000

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,52 +3,62 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const autoprefixer = require('autoprefixer');
+const inlineCss = require('gulp-inline-css');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 let uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync');
 
 let path = {
-    src_sass:'./src/scss/*.scss',
-    src_js:'./src/js/main.js',
+  src_sass: './src/scss/*.scss',
+  src_js: './src/js/main.js',
 }
 
-gulp.task("sassTask", function() {
-    return gulp
-        .src(path.src_sass)
-        .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
-        .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
-        .pipe(gulp.dest('./dist'))
-        .pipe(browserSync.stream());
+gulp.task("sassTask", function () {
+  return gulp
+    .src(path.src_sass)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
 });
 
 // JS task: uglifies JS files to main.js
-gulp.task('jsTask', function() {
-    return gulp
-      .src(path.src_js)
-      .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(sourcemaps.write()) // Inline source maps.
-      .pipe(gulp.dest('./dist'))
-      .pipe(browserSync.stream());
-  });
+gulp.task('jsTask', function () {
+  return gulp
+    .src(path.src_js)
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write()) // Inline source maps.
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+// //inlineCss
+gulp.task('inlineCss', function () {
+  return gulp.src('./*.html')
+    .pipe(inlineCss())
+    .pipe(gulp.dest('./dist'));
+});
+
+
 
 // Static Server & watching scss/js/html files
-gulp.task('serve', gulp.series('sassTask', function() {
-    browserSync.init({
-        server: "./"   
-    });
-  
-    gulp.watch(path.src_sass,
-      gulp.series('sassTask'));
+gulp.task('serve', gulp.series('sassTask', function () {
+  browserSync.init({
+    server: "./"
+  });
 
-    gulp.watch(path.src_js,
-      gulp.series("jsTask"));
+  gulp.watch(path.src_sass,
+    gulp.series('sassTask'));
 
-    gulp.watch("*.html").on('change', browserSync.reload);
-  }));
-  
-  gulp.task('default',
-    gulp.series('sassTask', "jsTask", 'serve'));
+  gulp.watch(path.src_js,
+    gulp.series("jsTask"));
+
+  gulp.watch("*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default',
+  gulp.series('sassTask', "jsTask", 'serve'));
